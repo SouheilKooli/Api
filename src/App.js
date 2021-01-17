d527db843392392911be96d4725a2e5300d261ce
Souheil Kooli <kooli_shouheil@yahoo.fr>
2021-01-17 12:51:16 +0100

    step2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from './Components/userList';
+
+
+function App() {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
